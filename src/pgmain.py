62c7687e96e865e461864ea164720ab8041191b9
Shameem Gafoor <shameem.gafoor@outlook.com>
2024-06-03 09:27:29 +0100

    Added implementation using pgvector

---
+++ src/pgmain.py
@@ -0,0 +1,57 @@
+from langchain_google_vertexai import ChatVertexAI
+
+
+import bs4
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI
+from langchain_postgres.vectorstores import PGVector
+
+
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+
+# Load, chunk and index the contents of the blog.
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+connection = "postgresql+psycopg://langchain:langchain@localhost:6024/langchain"  # Uses psycopg3!
+
+vectorstore = PGVector(
+    embeddings=OpenAIEmbeddings(),
+    collection_name='webtext',
+    connection=connection,
+    use_jsonb=True,
+)
+
+vectorstore.add_documents(documents=splits)
+# Retrieve and generate using the relevant snippets of the blog.
+retriever = vectorstore.as_retriever()
+prompt = hub.pull("rlm/rag-prompt")
+
+vectorstore.get_collection()
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+response = rag_chain.invoke("What is Task Decomposition?")
+print(f"Response: {response}")
